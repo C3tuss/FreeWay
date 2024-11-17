b12bae1bc6e6a6c4e5664db11d8337cff614d52e
C3tuss <alexandrevilela.sds@gmail.com>
2024-11-17 13:47:49 -0300

    adição do score na tela de fim de jogo e background transparente com carros de fundo

--- temporizador.js
+++ temporizador.js
@@ -1,5 +1,5 @@
 let tempoInicial;
-let duracao = 40 * 1000; // 40 segundos em milissegundos
+let duracao = 3 * 1000; // 40 segundos em milissegundos
 let tempoRestante;
 
 // Função para mostrar o temporizador
@@ -10,11 +10,30 @@ function incluiTemporizador() {
     text(`Tempo: ${ceil(tempoRestante / 1000)}s`, width / 2, 27);
 }
 
+function score(){
+    textAlign(CENTER);
+    textSize(25);
+    fill(color(255, 240, 60));
+    text(`SCORE: ${meusPontos}`, width/2, 250);
+}
+
 // Função para exibir a tela de fim de jogo
 function telaFimDeJogo() {
-    background(0, 0, 0, 200); // Fundo semi-transparente
+    
+    //Mostra os carros de fundo
+    background(imagemDaEstrada);
+    mostraCarro();
+    movimentaCaro();
+    voltaPosicaoInicialDoCarro();
+
+    // Sobreposição preta translúcida
+    fill(0, 0, 0, 200); // Preto com transparência
+    rect(0, 0, width, height);
+
+    // Mensagem de fim de jogo
     textAlign(CENTER);
     textSize(40);
     fill(color(255, 0, 0));
     text("Fim de Jogo!", width / 2, height / 2);
+    score();
 }
